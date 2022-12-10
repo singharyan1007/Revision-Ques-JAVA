0ec4ea47bc019ce549aaa887d71cb32f7011cb57
Aryan Singh <singh.aryan837@gmail.com>
2022-12-10 22:37:48 +0530

    linear search

---
+++ src/binarysearch/floorofanumber.java
@@ -0,0 +1,30 @@
+package binarysearch;
+
+public class floorofanumber {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,6,11,14,16,18};
+        int target=15;
+        int ans=search(arr,target);
+        System.out.println(ans);
+    }
+
+
+    static int search(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target==arr[mid]){
+                return arr[mid];
+            }
+            else if(target<arr[mid]){
+                end=mid-1;
+            }
+            else if(target>arr[mid]) {
+                start = mid + 1;
+            }
+        }
+        return arr[end];
+    }
+}
+
